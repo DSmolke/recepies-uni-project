20ec1c9ad08c88a3abb866f33bda6cf1399b546b
Damian_Smolczynski <d.smolczynski1@gmail.com>
2024-06-25 17:03:53 +0200

    Prototype of page layout and basic components creation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import MediaCard from "./components/MediaCard";
+import SearchBar from "./components/SearchBar";
+import Banner from "./components/Banner";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Test Prod <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => { return (
+    <>  <Banner/>
+        <SearchBar/>
+        <MediaCard/>
+    </>)
 }
-
-export default App;
+export default App
